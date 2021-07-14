350f897a8ba4fa86fca32ba4a982f032841751a7
Dustin Bailey <bailey.dust@gmail.com>
2021-07-13 17:08:19 -0700

    Change in new file

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ import reducer from './store/reducers/app';
 const store = createStore(
   reducer,
   applyMiddleware(logger)
-);
+);//updating difffernt file
 
 ReactDOM.render(<Provider store={store}><App /></Provider> , document.getElementById('root'));
 

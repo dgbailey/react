524ea1188a99b3e1b0ebd9de5d55234396e3c66c
Dustin Bailey <bailey.dust@gmail.com>
2021-07-14 12:10:57 -0700

    Email test 2

--- src/index.js
+++ src/index.js
@@ -14,6 +14,7 @@ const store = createStore(
   applyMiddleware(logger)
 );//changing unrelated file
 ///testing email linking
+//testing againg
 
 
 ReactDOM.render(<Provider store={store}><App /></Provider> , document.getElementById('root'));
